113dee65ca7853146d541eff1112dd34cf82da1f
Maicon Santos <matheus.binario@gmail.com>
2020-04-21 17:57:26 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/maiconsantos/cursomc/domain/ItemPedido.java
+++ src/main/java/com/maiconsantos/cursomc/domain/ItemPedido.java
@@ -5,14 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@JsonBackReference
+
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 
