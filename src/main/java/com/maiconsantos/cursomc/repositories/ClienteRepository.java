00181a25caf8ab660dbde64a3efffeba462d4cda
Maicon Santos <matheus.binario@gmail.com>
2020-04-22 16:25:55 -0300

    Validacao customizada: email nao repetido na insercao de Cliente.obs: tratar excep

--- src/main/java/com/maiconsantos/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/maiconsantos/cursomc/repositories/ClienteRepository.java
@@ -2,9 +2,15 @@ package com.maiconsantos.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.maiconsantos.cursomc.domain.Cliente;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
 
 }
--- src/main/java/com/maiconsantos/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/maiconsantos/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.maiconsantos.cursomc.domain.Cliente;
 import com.maiconsantos.cursomc.domain.enums.TipoCliente;
 import com.maiconsantos.cursomc.dto.ClienteNewDTO;
+import com.maiconsantos.cursomc.repositories.ClienteRepository;
 import com.maiconsantos.cursomc.resources.exception.FieldMessage;
 import com.maiconsantos.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -30,6 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
