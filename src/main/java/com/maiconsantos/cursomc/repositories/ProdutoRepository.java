2ea43b4b9095b8bfa207083d8a3f6dcb5aba2df2
Maicon Santos <matheus.binario@gmail.com>
2020-04-22 17:43:25 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/maiconsantos/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/maiconsantos/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,26 @@
 package com.maiconsantos.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.maiconsantos.cursomc.domain.Categoria;
 import com.maiconsantos.cursomc.domain.Produto;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	// vc pode usar o query ou o nome intuitivo do jeito que tá o query sobrescreve
+	// o metodo por nomes -ambos fazem a mesma coisa
+	// verificar sugestão de nomes na documentação do spring io
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,
+			@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
