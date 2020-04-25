5a1e2501c88f534cccb9632394c8e79699a3fd68
Maicon Santos <matheus.binario@gmail.com>
2020-04-25 18:09:33 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/maiconsantos/cursomc/services/PedidoService.java
+++ src/main/java/com/maiconsantos/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.maiconsantos.cursomc.domain.Cliente;
 import com.maiconsantos.cursomc.domain.ItemPedido;
 import com.maiconsantos.cursomc.domain.PagamentoComBoleto;
 import com.maiconsantos.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.maiconsantos.cursomc.domain.enums.EstadoPagamento;
 import com.maiconsantos.cursomc.repositories.ItemPedidoRepository;
 import com.maiconsantos.cursomc.repositories.PagamentoRepository;
 import com.maiconsantos.cursomc.repositories.PedidoRepository;
+import com.maiconsantos.cursomc.security.UserSS;
+import com.maiconsantos.cursomc.services.exception.AuthorizationException;
 import com.maiconsantos.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -31,6 +37,9 @@ public class PedidoService {
 
 	@Autowired
 	private ProdutoService produtoService;
+
+	@Autowired
+	private ClienteService clienteService;
 
 	@Autowired
 	private EmailService emailService;
@@ -60,5 +69,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 }
