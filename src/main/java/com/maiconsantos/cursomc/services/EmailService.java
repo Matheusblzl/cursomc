835d6c2d4c36f33a04e1bd46842ea177021cb5b1
Maicon Santos <matheus.binario@gmail.com>
2020-04-25 18:54:11 -0300

    Esqueci a senha

--- src/main/java/com/maiconsantos/cursomc/services/EmailService.java
+++ src/main/java/com/maiconsantos/cursomc/services/EmailService.java
@@ -2,13 +2,15 @@ package com.maiconsantos.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.maiconsantos.cursomc.domain.Cliente;
 import com.maiconsantos.cursomc.domain.Pedido;
-
 
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
